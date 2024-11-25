a437695c3837d9abd7f024c25d9d35730a3a72c4
Ocskó Gábor <ogabor@gmail.com>
2024-11-25 20:47:33 +0100

    feat: implement heatmaps component with data fetching

--- src/app/optimization-session-heatmaps/optimization-session-heatmaps.component.ts
+++ src/app/optimization-session-heatmaps/optimization-session-heatmaps.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OptimizationSessionResult } from '../optimization-session-results/optimization-session-result.model';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-optimization-session-heatmaps',
   standalone: true,
   imports: [],
   templateUrl: './optimization-session-heatmaps.component.html',
-  styleUrl: './optimization-session-heatmaps.component.scss'
+  styleUrl: './optimization-session-heatmaps.component.scss',
 })
 export class OptimizationSessionHeatmapsComponent {
+  sessionId = inject(ActivatedRoute).snapshot.params['sessionId'];
+  private httpClient = inject(HttpClient);
+  results = signal<OptimizationSessionResult[]>([]);
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.httpClient
+      .get<OptimizationSessionResult[]>(
+        `http://127.0.0.1:5000/optimization-sessions/${this.sessionId}`
+      )
+      .subscribe((data) => this.results.set(data));
+  }
+
+  sanitizeUrl(
+    ticker: string,
+    strategyId: string,
+    start: string,
+    end: string,
+    interval: string
+  ): SafeResourceUrl {
+    const url = `http://127.0.0.1:5000/reports/optimization/optimization_results_${ticker}_${strategyId}_${start}_${end}_${interval}.html`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
