81f545282011e1c69b20911f4ee94f9929955b07
Ocskó Gábor <ogabor@gmail.com>
2024-11-25 18:54:09 +0100

    feat: add loading spinner and integrate Angular Material for strategies component

--- src/app/strategies/strategies.component.ts
+++ src/app/strategies/strategies.component.ts
@@ -1,28 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Strategy } from './strategy.model';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-strategies',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule],
   templateUrl: './strategies.component.html',
   styleUrl: './strategies.component.scss',
 })
 export class StrategiesComponent implements OnInit {
   strategies = signal<Strategy[]>([]);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
+    this.isFetching.set(true);
     const subscription = this.httpClient
       .get<Strategy[]>('http://127.0.0.1:5000/strategies')
       .subscribe({
         next: (data) => {
           this.strategies.set(data);
         },
+        complete: () => {
+          this.isFetching.set(false);
+        },
         error: (error) => {
           console.error(error);
+          this.isFetching.set(false);
         },
       });
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
