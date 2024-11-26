8d3847688a07ae11a124ca3ef8fbe6d6fd66eabc
Ocskó Gábor <ogabor@gmail.com>
2024-11-26 20:21:47 +0100

    feat: enhance menu component with icons and layout adjustments; update styles for improved alignment and spacing

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
