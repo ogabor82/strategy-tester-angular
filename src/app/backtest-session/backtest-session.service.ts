b845d57516a67fbf39b3cd825502e9163cd1fac9
Ocskó Gábor <ogabor@gmail.com>
2024-11-27 16:04:53 +0100

    feat: implement BacktestSessionService and integrate backtest functionality into session configuration component

---
+++ src/app/backtest-session/backtest-session.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BacktestSession } from '../backtest-sessions/backtest-session.model';
+import { SessionConfiguration } from '../session-configuration/session-configuration.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BacktestSessionService {
+  constructor(private http: HttpClient) {}
+
+  runBacktest(sessionConfiguration: SessionConfiguration) {
+    const backtestRequest = {
+      strategy: sessionConfiguration.strategy,
+      tickers: sessionConfiguration.tickerSet?.tickers,
+      timeframe_set: sessionConfiguration.timeframeSet,
+      selected_session: sessionConfiguration.backtestSession,
+      backtest_results: 'compact',
+      backtest_plot: true,
+    };
+    return this.http.post<BacktestSession>(
+      'http://127.0.0.1:5000/run-backtest',
+      backtestRequest
+    );
+  }
+}
--- src/app/session-configuration/session-configuration.component.ts
+++ src/app/session-configuration/session-configuration.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SessionConfigurationService } from './session-configuratio.service';
 import { SessionConfiguration } from './session-configuration.model';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BacktestSessionService } from '../backtest-session/backtest-session.service';
 @Component({
   selector: 'app-session-configuration',
   standalone: true,
-  imports: [MatChipsModule, MatIconModule],
+  imports: [MatChipsModule, MatIconModule, MatButtonModule],
   templateUrl: './session-configuration.component.html',
   styleUrl: './session-configuration.component.scss',
 })
 export class SessionConfigurationComponent {
+  private backtestSessionService = inject(BacktestSessionService);
   constructor(
     private sessionConfigurationService: SessionConfigurationService
   ) {}
 
   get sessionConfiguration(): SessionConfiguration {
     return this.sessionConfigurationService.getSessionConfiguration();
+  }
+
+  runBacktest() {
+    this.backtestSessionService
+      .runBacktest(this.sessionConfiguration)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => console.error(error),
+      });
   }
 }
